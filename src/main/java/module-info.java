a3b580a060c1bd6d457c7b2aea92ec1b46783d29
JuanFrancisco21 <juanfranciscoaguilarsanchez@gmail.com>
2021-12-11 20:00:54 +0100

    Apertura de carpetas para javafx

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,6 @@
 module controller {
+	requires javafx.graphics;
+	requires javafx.base;
     requires javafx.controls;
     requires javafx.fxml;
 	requires java.persistence;
@@ -7,8 +9,9 @@ module controller {
     requires org.hibernate.commons.annotations;
 
 	opens model.DataObject to org.hibernate.commons.annotations, org.hibernate.orm.core, java.persistence;
-    opens controller to org.hibernate.commons.annotations, org.hibernate.orm.core, javafx.fxml, java.persistence;
+    opens controller to javafx.graphics,javafx.base, org.hibernate.commons.annotations, org.hibernate.orm.core, javafx.fxml, java.persistence;
     opens utils to org.hibernate.commons.annotations, org.hibernate.orm.core, java.persistence;
     exports controller;
+    exports model.DataObject;
 
 }
