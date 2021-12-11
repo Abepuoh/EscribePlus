b43cf6737f93e333ba53af75fee2d3d8ca358f00
Miguel Angel <dammiguelangel@gmail.com>
2021-12-11 18:03:16 +0100

    TransferDB

--- src/main/java/controller/Ejecutable.java
+++ src/main/java/controller/Ejecutable.java
@@ -11,7 +11,6 @@ import model.DAO.PartesDAO;
 import model.DAO.PersonajeDAO;
 import model.DAO.UsuarioDAO;
 import model.DataObject.Libro;
-import model.DataObject.Partes;
 import model.DataObject.Personaje;
 import model.DataObject.Usuario;
 import utils.ConnectionUtil;
@@ -20,44 +19,32 @@ public class Ejecutable {
 
 	public static void main(String[] args) {
 		EntityManager em = createEM();
-		em.getTransaction().begin();
-		// LOS CAMBIOS SON EFECTIVOS
-		UsuarioDAO a = new UsuarioDAO();
-		LibroDAO l = new LibroDAO();
-		PersonajeDAO p = new PersonajeDAO();
-		PartesDAO pt = new PartesDAO();
-		
-		/*Usuario user1 = new Usuario("Antonio", "1234", "prueba@prueba.com");
-		List<Personaje> characters = new ArrayList<Personaje>();
-		Libro book1 = new Libro("Quijote",1995,"Un jambo que va to puesteo", "Fantasía", user1, null);
-		List<Libro> books = new ArrayList<Libro>();
-		Personaje character1 = new Personaje("Sancho",18,"Señor con panza","Malo" ,"Notas", books);
-		characters.add(character1);
+        em.getTransaction().begin();
+        // LOS CAMBIOS SON EFECTIVOS
+        UsuarioDAO a = new UsuarioDAO();
+        LibroDAO l = new LibroDAO();
+        PersonajeDAO p = new PersonajeDAO();
+        PartesDAO pt = new PartesDAO();
 
-		book1.setPersonajes(characters);
-		books.add(book1);
-		user1.setBooks(books);*/
+        Usuario user1 = new Usuario("Carlos", "1234", "prueba@prueba.com");
+        List<Personaje> characters = new ArrayList<Personaje>();
+        Libro book1 = new Libro("Quijote",1995,"Fantasía", "Un jambo que va to puesteo", user1, characters);
+        List<Libro> books = new ArrayList<Libro>();
+        Personaje character1 = new Personaje("Sancho",18,"Señor con panza","Malo" ,"Notas", books);
+        characters.add(character1);
 
-		//p.crear(character1);
-		//a.crear(user1);
-		//l.crear(book1);
+        book1.setPersonajes(characters);
+        books.add(book1);
+        user1.setBooks(books);
 
-		// em.persist(user1);
-
-		//Usuario aux = em.find(Usuario.class, 1L);
-		
-		//Partes x = pt.getById(1L); 
-		Libro q = l.getBookByName("Quijote");
-		//q.addParte(new Partes("Primera parte", q));
-		//l.editar(q);
-		System.out.println(q.getParts());
-		/*System.out.println(user1);
-		System.out.println(aux);
-		System.out.println(book1);*/
-
+       a.crear(user1);
 		// FINAL DE LOS CAMBIOS
 		///////////////////////////////////////////////////////////////////////////
 		em.getTransaction().commit();
+		
+
+		ConnectionUtil.transferH2ToMariaDB();
+
 
 
 	}
--- src/main/java/utils/ConnectionUtil.java
+++ src/main/java/utils/ConnectionUtil.java
@@ -1,14 +1,28 @@
 package utils;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.PersistenceException;
+import javax.persistence.TypedQuery;
+
+import model.DataObject.Libro;
+import model.DataObject.Notas_Cap;
+import model.DataObject.Notas_Lib;
+import model.DataObject.Partes;
+import model.DataObject.Personaje;
+import model.DataObject.Recordatorio;
+import model.DataObject.Usuario;
 
 public class ConnectionUtil {
 
 	private static EntityManagerFactory emf;
 	public static boolean choice = true; // ----->TRUE LOCAL ------> FALSE NUBE
 	private static final String LOCAL = "AplicacionH2";
-	private static final String REMOTE = "AplicacionMariaDB";
+	private static final String REMOTE = "aplicacionMariaDB";
 
 	public static EntityManagerFactory getInstance(String name) {
 		if (emf == null) {
@@ -18,21 +32,14 @@ public class ConnectionUtil {
 		}
 		return emf;
 	}
-/*
-	public static EntityManagerFactory getRemoteInstance() {
-		if (emf == null) {
-			emf = Persistence.createEntityManagerFactory(REMOTE);
-		}
-		return emf;
-	}
 
-	public static EntityManagerFactory getLocalInstance() {
-		if (emf == null) {
-			emf = Persistence.createEntityManagerFactory(LOCAL);
-		}
-		return emf;
-	}
-*/
+	/*
+	 * public static EntityManagerFactory getRemoteInstance() { if (emf == null) {
+	 * emf = Persistence.createEntityManagerFactory(REMOTE); } return emf; }
+	 * 
+	 * public static EntityManagerFactory getLocalInstance() { if (emf == null) {
+	 * emf = Persistence.createEntityManagerFactory(LOCAL); } return emf; }
+	 */
 	public static EntityManagerFactory getInstance() {
 		if (emf == null) {
 			if (choice) {
@@ -42,5 +49,108 @@ public class ConnectionUtil {
 			}
 		}
 		return emf;
+	}
+
+	public static void transferH2ToMariaDB() throws IllegalStateException {
+		if (emf != null) {
+			EntityManagerFactory temp = null;
+			try {
+				temp = Persistence.createEntityManagerFactory("aplicacionMariaDB");
+			} catch (PersistenceException e) {
+				throw new IllegalStateException(e.getMessage());
+			}
+			EntityManager t = temp.createEntityManager();
+			t.getTransaction().begin();
+
+			for (Usuario userAux : getAllUsers(emf)) {
+				userAux = t.merge(userAux);
+				t.persist(userAux);
+			}
+			for (Libro libroAux : getAllBooks(emf)) {
+				libroAux = t.merge(libroAux);
+				t.persist(libroAux);
+			}
+			for (Partes partesAux : getAllPartes(emf)) {
+				partesAux = t.merge(partesAux);
+				t.persist(partesAux);
+			}
+			for (Notas_Cap notCapAux: getAllNotas_Cap(emf)) {
+				notCapAux = t.merge(notCapAux);
+				t.persist(notCapAux);
+			}
+			for (Notas_Lib notLibAux : getAllNotas_Lib(emf)) {
+				notLibAux = t.merge(notLibAux);
+				t.persist(notLibAux);
+			}
+			for (Personaje personajeAux : getAllPersonajes(emf)) {
+				personajeAux = t.merge(personajeAux);
+				t.persist(personajeAux);
+			}
+			for (Recordatorio recordatorioAux : getAllRecordatorios(emf)) {
+				recordatorioAux = t.merge(recordatorioAux);
+				t.persist(recordatorioAux);
+			}
+			t.getTransaction().commit();
+			t.close();
+			temp.close();
+		}
+	}
+
+	private static List<Usuario> getAllUsers(EntityManagerFactory emf) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		TypedQuery<Usuario> q = em.createNamedQuery("getAllUsers", Usuario.class);
+		List<Usuario> result = new ArrayList<>(q.getResultList());
+		em.getTransaction().commit();
+		return result;
+	}
+
+	public static List<Libro> getAllBooks(EntityManagerFactory emf) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		TypedQuery<Libro> q = em.createQuery("getAllBooks", Libro.class);
+		List<Libro> result = new ArrayList<>(q.getResultList());
+		em.getTransaction().commit();
+		return result;
+	}
+	public static List<Partes> getAllPartes(EntityManagerFactory emf) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		TypedQuery<Partes> q = em.createQuery("getAllPartes", Partes.class);
+		List<Partes> result = new ArrayList<>(q.getResultList());
+		em.getTransaction().commit();
+		return result;
+	}
+	public static List<Notas_Cap> getAllNotas_Cap(EntityManagerFactory emf) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		TypedQuery<Notas_Cap> q = em.createQuery("getAllNotas_Cap", Notas_Cap.class);
+		List<Notas_Cap> result = new ArrayList<>(q.getResultList());
+		em.getTransaction().commit();
+		return result;
+	}
+	public static List<Notas_Lib> getAllNotas_Lib(EntityManagerFactory emf) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		TypedQuery<Notas_Lib> q = em.createQuery("getAllNotas_Lib", Notas_Lib.class);
+		List<Notas_Lib> result = new ArrayList<>(q.getResultList());
+		em.getTransaction().commit();
+		return result;
+	}
+	public static List<Personaje> getAllPersonajes(EntityManagerFactory emf) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		TypedQuery<Personaje> q = em.createQuery("getAllPersonajes", Personaje.class);
+		List<Personaje> result = new ArrayList<>(q.getResultList());
+		em.getTransaction().commit();
+		return result;
+	}
+	public static List<Recordatorio> getAllRecordatorios(EntityManagerFactory emf) {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		TypedQuery<Recordatorio> q = em.createQuery("getAllRecordatorios", Recordatorio.class);
+		List<Recordatorio> result = new ArrayList<>(q.getResultList());
+		em.getTransaction().commit();
+		return result;
 	}
 }
