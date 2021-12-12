ff9149a5078ad9ce8c102b12d7d0cf48c8fda209
Miguel Angel <dammiguelangel@gmail.com>
2021-12-12 20:18:05 +0100

    edit

--- src/main/java/controller/CapitulosController.java
+++ src/main/java/controller/CapitulosController.java
@@ -7,10 +7,15 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 import model.DAO.CapituloDAO;
 import model.DAO.Notas_CapDAO;
 import model.DAO.PartesDAO;
@@ -45,66 +50,96 @@ public class CapitulosController {
     @FXML
     private Button buttEditarNota;
     
+
+
     private static Libro milibro;
     private PartesDAO parDao=new PartesDAO();
     private CapituloDAO cadao=new CapituloDAO();
     private Notas_CapDAO ndao=new Notas_CapDAO();
-    
+
     public void initialize() {
-    	milibro=MainLibrosController.currentBook;
-    	configuraTablas();
-    	CBParte.getItems().addAll(parDao.getByLibro(milibro));
-    	CBParte.getSelectionModel().selectedItemProperty().addListener((Observable,oldValue,newValue)->{
-    		tablacapitulos(newValue);
-    	});
-    	TVCapitulos.getSelectionModel().selectedItemProperty().addListener((Observable,oldValue,newValue)->{
-    		tablaNotas(newValue);
-    	});
+        milibro=MainLibrosController.currentBook;
+        configuraTablas();
+        CBParte.getItems().addAll(parDao.getByLibro(milibro));
+        CBParte.getSelectionModel().selectedItemProperty().addListener((Observable,oldValue,newValue)->{
+            tablacapitulos(newValue);
+        });
+        TVCapitulos.getSelectionModel().selectedItemProperty().addListener((Observable,oldValue,newValue)->{
+            tablaNotas(newValue);
+        });
     }
-    
+
     public void configuraTablas() {
-    	TCCapitulosNombre.setCellValueFactory(cadenaCapNombre->
-    		new SimpleStringProperty(cadenaCapNombre.getValue().getName()));
-    	
-    	TCDescripcion.setCellValueFactory(cadenaCapNombre->
-		new SimpleStringProperty(cadenaCapNombre.getValue().getText()));
-    	
-    	
-    	
+        TCCapitulosNombre.setCellValueFactory(cadenaCapNombre->
+            new SimpleStringProperty(cadenaCapNombre.getValue().getName()));
+
+        TCDescripcion.setCellValueFactory(cadenaCapNombre->
+        new SimpleStringProperty(cadenaCapNombre.getValue().getText()));
+
+
+
     }
     private void tablacapitulos(Partes p) {
-    	List<Capitulo> capitulos= cadao.getByParte(p);
-    	TVCapitulos.setItems(FXCollections.observableList(capitulos));
-    	TVCapitulos.refresh();
+        List<Capitulo> capitulos= cadao.getByParte(p);
+        TVCapitulos.setItems(FXCollections.observableList(capitulos));
+        TVCapitulos.refresh();
     }
     private void tablaNotas(Capitulo c) {
-    	List<Notas_Cap> notas= ndao.getFromCapitulos(c);
-    	TVNotas.setItems(FXCollections.observableList(notas));
-    	TVNotas.refresh();
-    	
+        List<Notas_Cap> notas= ndao.getFromCapitulos(c);
+        TVNotas.setItems(FXCollections.observableList(notas));
+        TVNotas.refresh();
+
     }
-    
+
     @FXML
     void editActos(ActionEvent event) throws IOException {
     	
+    	FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("EditPartes.fxml"));
+		Parent modal;
+		try {
+			modal = fxmlLoader.load();
+			Stage modalStage = new Stage();
+			modalStage.initModality(Modality.APPLICATION_MODAL);
+			modalStage.initOwner(App.rootstage);
+			Scene modalScene = new Scene(modal);
+			modalStage.setScene(modalScene);
+			modalStage.showAndWait();
+			modalStage.setResizable(false);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
 
     @FXML
     void editarCap(ActionEvent event) throws IOException{
-    	
+
     }
 
     @FXML
     void editarNota(ActionEvent event) throws IOException{
-    	
+
+    	FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("EditNotasCap.fxml"));
+		Parent modal;
+		try {
+			modal = fxmlLoader.load();
+			Stage modalStage = new Stage();
+			modalStage.initModality(Modality.APPLICATION_MODAL);
+			modalStage.initOwner(App.rootstage);
+			Scene modalScene = new Scene(modal);
+			modalStage.setScene(modalScene);
+			modalStage.showAndWait();
+			modalStage.setResizable(false);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
     }
-    
+
     @FXML
     private void switchToLibro() throws IOException {
         App.setRoot("MainLibros");
     }
     @FXML
     public void closeApp() {
-    	System.exit(0);
+        System.exit(0);
     }
 }
---
+++ src/main/java/controller/EditNotasCap.java
@@ -0,0 +1,66 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+public class EditNotasCap {
+
+    @FXML
+    private ChoiceBox<?> CBCREARNLibros;
+
+    @FXML
+    private ChoiceBox<?> CBEditNLibros;
+
+    @FXML
+    private ChoiceBox<?> CBEditNotas;
+
+    @FXML
+    private ChoiceBox<?> CBRemoveNLibro;
+
+    @FXML
+    private ChoiceBox<?> CBRemoveNota;
+
+    @FXML
+    private TextField TFCrearNDescripcion;
+
+    @FXML
+    private TextField TFNotaDescripcion;
+
+    @FXML
+    private Button buttBorrar;
+
+    @FXML
+    private Button buttCrear;
+
+    @FXML
+    private Button buttEditarNota;
+
+    @FXML
+    void borrarNota(ActionEvent event) {
+
+    }
+
+    @FXML
+    void closeApp(ActionEvent event) {
+
+    }
+
+    @FXML
+    void crearNota(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editarNota(ActionEvent event) {
+
+    }
+
+    @FXML
+    void switchToLibro(ActionEvent event) {
+
+    }
+
+}
